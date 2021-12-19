666a82f5a3d9580513cfe9d0b1c2a4d569d0b759
Earvin Lin <lin.earvin@gmail.com>
2021-12-19 17:18:51 +0800

    20211219 Add files about CPP6th-ch13

---
+++ testpgms/CPP6th/ch13/testOpenFile.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int ch;
+    FILE *fp;
+    unsigned long count = 0;
+
+    if ((fp = fopen(argv[1], "rt")) == NULL) {
+        printf("Can't open %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    while ((ch = getc(fp)) != EOF) {
+        putc(ch, stdout);
+    }
+
+    fclose(fp);
+    printf("File %s read finished!!\n", argv[1]);
+
+    return 0;
+}
+
